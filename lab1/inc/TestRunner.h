86fda1db68990a791c115be287c43f9b0521f29f
FoF312 <bulonv786@gmail.com>
2025-03-14 22:09:32 +0200

    lab1 fix

--- lab1/inc/TestRunner.h
+++ lab1/inc/TestRunner.h
@@ -2,16 +2,9 @@
 
 class TestRunner {
 public:
-    // Запуск всех тестов
     void runAllTests();
-
-    // Запуск всех функциональных тестов
     void runFunctionalTests();
-
-    // Запуск всех логических тестов
     void runLogicalTests();
-
-    // Запуск отдельных тестов
     void runAppendTest();
     void runRemovePrimesTest();
     void runSortTest();
