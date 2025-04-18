15279640dee1cd3cb8f17eef81c56cb1a57c0e6d
FoF312 <bulonv786@gmail.com>
2025-04-18 21:33:04 +0200

    111

--- lab2/inc/TaskExecutor.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class TaskExecutor {
-public:
-    static void printAssignment();
-    static void runReverseNumberTask();
-    static void runQuickSortTask();
-    static void runPathFinderTask();
-};
--- lab4/inc/Tests/Functional/BigIntFunctionalTests.h
+++
@@ -1,9 +0,0 @@
-#ifndef BIGINTFUNCTIONALTESTS_H
-#define BIGINTFUNCTIONALTESTS_H
-
-class BigIntFunctionalTests {
-public:
-    static void runAllTests();
-};
-
-#endif // BIGINTFUNCTIONALTESTS_H
